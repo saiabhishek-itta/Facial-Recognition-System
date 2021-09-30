2726a8efa461fdee92a7481cca5c267b1d4acef0
Sai Abhishek <abishekitta@gmail.com>
2021-09-30 11:43:22 +0530

    1

--- work.py
+++ work.py
@@ -202,8 +202,12 @@ def facultypanel():
         facultypanel.tv.heading('name',text ='NAME')
         facultypanel.tv.heading('date',text ='DATE')
         facultypanel.tv.heading('time',text ='TIME')
+        facultypanel.tkslot = tk.Entry(admin,width=32 ,fg="black",font=('times', 15, ' bold ')  )
+        facultypanel.tkslot.place(x=600, y=173)
         tknewregistrationbtn = tk.Button(admin, text="Take Attendence",command=TrackImages,fg="black"  ,bg="#ea2a2a"  ,width=11 ,activebackground = "white" ,font=('times', 11, ' bold '))
         tknewregistrationbtn.place(x=700, y=315)
+        facultypanel.fperror = tk.Label(admin, text="error!",bg="#262523", width=80, fg="red",  height=1, font=('times', 15, ' bold '))
+        facultypanel.fperror.place(x=300, y=415)
         window.destroy()
         admin.mainloop()
     else:
@@ -211,86 +215,90 @@ def facultypanel():
         print("Faculty login failed")
 
 def TrackImages():
-    check_haarcascadefile()
-    assure_path_exists("Attendance/")
-    assure_path_exists("StudentDetails/")
-    for k in facultypanel.tv.get_children():
-        facultypanel.tv.delete(k)
-    msg = ''
-    i = 0
-    j = 0
-    recognizer = cv2.face.LBPHFaceRecognizer_create()  # cv2.createLBPHFaceRecognizer()
-    exists3 = os.path.isfile("TrainingImageLabel\Trainner.yml")
-    if exists3:
-        recognizer.read("TrainingImageLabel\Trainner.yml")
+    if(facultypanel.tkslot.get()==""):
+        facultypanel.fperror.config(text="Enter Slot!")
     else:
-        mess._show(title='Data Missing', message='Please click on Save Profile to reset data!!')
-        return
-    harcascadePath = "haarcascade_frontalface_default.xml"
-    faceCascade = cv2.CascadeClassifier(harcascadePath)
+        print("into else")
+        check_haarcascadefile()
+        assure_path_exists("Attendance/")
+        assure_path_exists("StudentDetails/")
+        for k in facultypanel.tv.get_children():
+            facultypanel.tv.delete(k)
+        msg = ''
+        i = 0
+        j = 0
+        recognizer = cv2.face.LBPHFaceRecognizer_create()  # cv2.createLBPHFaceRecognizer()
+        exists3 = os.path.isfile("TrainingImageLabel\Trainner.yml")
+        if exists3:
+            recognizer.read("TrainingImageLabel\Trainner.yml")
+        else:
+            mess._show(title='Data Missing', message='Please click on Save Profile to reset data!!')
+            return
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        faceCascade = cv2.CascadeClassifier(harcascadePath)
 
-    cam = cv2.VideoCapture(0)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    col_names = ['Id', '', 'Name', '', 'Date', '', 'Time']
-    exists1 = os.path.isfile("StudentDetails\StudentDetails.csv")
-    if exists1:
-        df = pd.read_csv("StudentDetails\StudentDetails.csv")
-    else:
-        mess._show(title='Details Missing', message='Students details are missing, please check!')
+        cam = cv2.VideoCapture(0)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        col_names = ['Id', 'Name', 'Date', 'Time']
+        exists1 = os.path.isfile("StudentDetails\StudentDetails.csv")
+        if exists1:
+            df = pd.read_csv("StudentDetails\StudentDetails.csv")
+        else:
+            mess._show(title='Details Missing', message='Students details are missing, please check!')
+            cam.release()
+            cv2.destroyAllWindows()
+            window.destroy()
+        while True:
+            ret, im = cam.read()
+            gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(gray, 1.2, 5)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(im, (x, y), (x + w, y + h), (225, 0, 0), 2)
+                serial, conf = recognizer.predict(gray[y:y + h, x:x + w])
+                if (conf < 50):
+                    ts = time.time()
+                    date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
+                    timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                    aa = df.loc[df['SERIAL NO.'] == serial]['NAME'].values
+                    ID = df.loc[df['SERIAL NO.'] == serial]['ID'].values
+                    ID = str(ID)
+                    ID = ID[1:-1]
+                    bb = str(aa)
+                    bb = bb[2:-2]
+                    attendance = [str(ID), bb, str(date), str(timeStamp)]
+
+                else:
+                    Id = 'Unknown'
+                    bb = str(Id)
+                cv2.putText(im, str(bb), (x, y + h), font, 1, (255, 255, 255), 2)
+            cv2.imshow('Taking Attendance', im)
+            if (cv2.waitKey(1) == ord('q')):
+                break
+        ts = time.time()
+        date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
+        exists = os.path.isfile("Attendance\Attendance_"+facultypanel.tkslot.get()+"_"+ date + ".csv")
+        if exists:
+            with open("Attendance\Attendance_"+facultypanel.tkslot.get()+"_"+ date + ".csv", 'a+') as csvFile1:
+                writer = csv.writer(csvFile1)
+                writer.writerow(attendance)
+            csvFile1.close()
+        else:
+            with open("Attendance\Attendance_" +facultypanel.tkslot.get()+"_"+ date + ".csv", 'a+') as csvFile1:
+                writer = csv.writer(csvFile1)
+                writer.writerow(col_names)
+                writer.writerow(attendance)
+            csvFile1.close()
+        with open("Attendance\Attendance_" +facultypanel.tkslot.get()+"_"+ date + ".csv", 'r') as csvFile1:
+            reader1 = csv.reader(csvFile1)
+            for lines in reader1:
+                i = i + 1
+                if (i > 1):
+                    if (i % 2 != 0):
+                        iidd = str(lines[0]) + '   '
+                        facultypanel.tv.insert('', 0, text=iidd, values=(str(lines[1]), str(lines[2]), str(lines[3])))
+        csvFile1.close()
         cam.release()
         cv2.destroyAllWindows()
-        window.destroy()
-    while True:
-        ret, im = cam.read()
-        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray, 1.2, 5)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(im, (x, y), (x + w, y + h), (225, 0, 0), 2)
-            serial, conf = recognizer.predict(gray[y:y + h, x:x + w])
-            if (conf < 50):
-                ts = time.time()
-                date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
-                timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
-                aa = df.loc[df['SERIAL NO.'] == serial]['NAME'].values
-                ID = df.loc[df['SERIAL NO.'] == serial]['ID'].values
-                ID = str(ID)
-                ID = ID[1:-1]
-                bb = str(aa)
-                bb = bb[2:-2]
-                attendance = [str(ID), '', bb, '', str(date), '', str(timeStamp)]
-
-            else:
-                Id = 'Unknown'
-                bb = str(Id)
-            cv2.putText(im, str(bb), (x, y + h), font, 1, (255, 255, 255), 2)
-        cv2.imshow('Taking Attendance', im)
-        if (cv2.waitKey(1) == ord('q')):
-            break
-    ts = time.time()
-    date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
-    exists = os.path.isfile("Attendance\Attendance_" + date + ".csv")
-    if exists:
-        with open("Attendance\Attendance_" + date + ".csv", 'a+') as csvFile1:
-            writer = csv.writer(csvFile1)
-            writer.writerow(attendance)
-        csvFile1.close()
-    else:
-        with open("Attendance\Attendance_" + date + ".csv", 'a+') as csvFile1:
-            writer = csv.writer(csvFile1)
-            writer.writerow(col_names)
-            writer.writerow(attendance)
-        csvFile1.close()
-    with open("Attendance\Attendance_" + date + ".csv", 'r') as csvFile1:
-        reader1 = csv.reader(csvFile1)
-        for lines in reader1:
-            i = i + 1
-            if (i > 1):
-                if (i % 2 != 0):
-                    iidd = str(lines[0]) + '   '
-                    facultypanel.tv.insert('', 0, text=iidd, values=(str(lines[2]), str(lines[4]), str(lines[6])))
-    csvFile1.close()
-    cam.release()
-    cv2.destroyAllWindows()
  
 
 ########################################################################################################################################
